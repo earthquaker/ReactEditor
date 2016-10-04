bf726d7716845ef9a1678e9249d4063587e137c6
Elias Hansen <pe_lias@msn.com>
2016-10-04 18:27:02 +0200

    added codemirror

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Codemirror from 'react-codemirror';
+import "codemirror/lib/codemirror.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: ''
+        };
+    }
+    updateCode(newCode) {
+        this.setState({
+            code: newCode
+        });
+    }
+    render() {
+        let options = {
+            lineNumbers: true,
+            mode: 'javascript'
+        };
+        return (
+            <div className="App">
+                <Codemirror value={this.state.code} onChange={()=>this.updateCode} options={options} />
+            </div>
+        );
+    }
 }
 
 export default App;
