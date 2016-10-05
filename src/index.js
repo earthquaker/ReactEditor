5ad597b55f127a165082f866fbe647eeb80e29b3
earthquaker <pe_lias@msn.com>
2016-10-05 10:48:11 +0200

    added firebase and setup firebase config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import App from './App';
 import './styles/index.css';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBeThZ8uvX1LcLq3rCyKhPRCBsS8Aa8Ud4",
+    authDomain: "editor-8d808.firebaseapp.com",
+    databaseURL: "https://editor-8d808.firebaseio.com",
+    storageBucket: "editor-8d808.appspot.com",
+    messagingSenderId: "95238276998"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+console.log(firebase);
